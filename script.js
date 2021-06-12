54377d3a279ab31ade89ad0c2f77c27a4a103ee3
Johnathan James <jmjames673@gmail.com>
2021-06-11 22:29:43 -0400

    Created function that changes color of individual squares to a darker grey when moused over.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const container = document.createElement("div");
 container.id = "main-container";
 body.appendChild(container);
 
-// heading 
+// heading
 
 const h1 = document.createElement("h1");
 h1.textContent = "Sketch An Etch";
@@ -21,14 +21,12 @@ gridContainer.style.flexWrap = "wrap";
 gridContainer.style.width = "50px";
 gridContainer.style.height = "50px";
 gridContainer.style.margin = "auto";
-// gridContainer.style.position = "absolute";
-// gridContainer.style.left = "50%";
-// gridContainer.style.top = "50%";
 container.appendChild(gridContainer);
+
+// grid size inquiry function
 
 let gridSize = () => {
   let res = prompt("How many squares per side for this grid?");
-
   while (!(res >= 1 && res <= 100)) {
     let res = prompt("Number must be from 1 to 100.");
     return res;
@@ -36,35 +34,7 @@ let gridSize = () => {
   return res;
 };
 
-// console.log(gridSize());
-
-// square div template
-
-// const squareDiv = document.createElement("div");
-// squareDiv.style.height = "50px";
-// squareDiv.style.width = "50px";
-// squareDiv.style.backgroundColor = "dodgerblue";
-// gridContainer.appendChild(squareDiv);
-
-// const anotherDiv = document.createElement("div");
-// anotherDiv.style.height = "50px";
-// anotherDiv.style.width = "50px";
-// anotherDiv.style.backgroundColor = "rebeccapurple";
-// gridContainer.appendChild(anotherDiv);
-
-// const thirdDiv = document.createElement("div");
-// thirdDiv.style.height = "50px";
-// thirdDiv.style.width = "50px";
-// thirdDiv.style.backgroundColor = "tomato";
-// gridContainer.appendChild(thirdDiv);
-
-// const lastDiv = document.createElement("div");
-// lastDiv.style.height = "50px";
-// lastDiv.style.width = "50px";
-// lastDiv.style.backgroundColor = "forestgreen";
-// gridContainer.appendChild(lastDiv);
-
-// add square
+// square box creator function
 
 function createSquare(parent) {
   const squareDiv = document.createElement("div");
@@ -76,26 +46,34 @@ function createSquare(parent) {
   parent.appendChild(squareDiv);
 }
 
+// grid creator function
+
 let gridCreator = (gridSize) => {
-  gridContainer.style.width = `${(gridSize * 50) + (gridSize*4)}px`;
-
-  // for loop needs to add blocks based on grid size
-  // if 2, this means two blocks for width, and two blocks for height
-  // 3, for 3 blocks wide and 3 blocks tall (blocks need to fill square)
-
+  gridContainer.style.width = `${gridSize * 50 + gridSize * 4}px`;
   for (i = 0; i < gridSize; i++) {
     createSquare(gridContainer);
-    console.log(i);
-    console.log('square created in outer loop');
-    if (gridContainer.style.width != gridContainer.style.height) {      
+    if (gridContainer.style.width != gridContainer.style.height) {
       for (j = 1; j < gridSize; j++) {
         createSquare(gridContainer);
-        console.log('square created in inner loop');        
-    }    
+      }
+    }
   }
-}
-  gridContainer.style.height = `${(gridSize * 50) + (gridSize*4)}px`;
+  gridContainer.style.height = `${gridSize * 50 + gridSize * 4}px`;
+};
 
-}
+// mouseover function
+
+const divs = document.querySelectorAll('div');
+divs.forEach((div) => {
+  div.addEventListener('mouseover', function (e){
+    console.log(e.target.className);
+    console.log('firing')
+    if (e.target.className == "square-box") {
+      // change the box to darker grey
+      e.target.style.backgroundColor = '#979797';
+    }
+  });
+  
+});
 
 gridCreator(16);
